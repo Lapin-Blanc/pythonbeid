e92bf4d01d7b93cdd3dc0bd6eb73aef5e3bdbc10
Lapin-Blanc <fabien.toune@gmail.com>
2016-03-12 13:44:18 +0100

    Attach read_info method to smartcard.Card.Card instance instead of subclassing it

--- beid.py
+++ beid.py
@@ -1,6 +1,7 @@
 from smartcard.CardMonitoring import CardObserver, CardMonitor
 from smartcard.System import readers
 import smartcard
+from types import MethodType
 
 MAP_MOIS = {
     "JANV" : "01",
@@ -30,117 +31,113 @@ ID = [0x3F, 0x00, 0xDF, 0x01, 0x40, 0x31]
 ADDRESS = [0x3F, 0x00, 0xDF, 0x01, 0x40, 0x33]
 PHOTO = [0x3F, 0x00, 0xDF, 0x01, 0x40, 0x35]
 
-class BeidCard(smartcard.Card.Card):
-    def __init__(self, reader, card):
-        super().__init__(reader, card.atr)
-
-    def read_infos(self, read_photo=False):
-        self._cnx = self.createConnection()
-        self._cnx.connect()
-        # select file : informations
-        # TODO : manage return codes
-        cmd = [0x00, 0xA4, 0x08, 0x0C, len(ID)] + ID        
-        data, sw1, sw2 = self._sendADPU(cmd)
-    
-        # read file
-        cmd = [0x00, 0xB0, 0x00, 0x00, 256]
-        data, sw1, sw2 = self._sendADPU(cmd)
-        if "%x"%sw1 == "6c":
-            cmd = [0x00, 0xB0, 0x00, 0x00, sw2]
-            data, sw1, sw2 = self._sendADPU(cmd)
-            idx = 0
-            num_info = 0
-            infos = []
-            while num_info <= 12:
-                num_info = data[idx]
-                idx += 1
-                len_info = data[idx]
-                idx += 1
-                chaine_bytes = []
-                for x in range(len_info):
-                    chaine_bytes.append(data[idx])
-                    idx += 1
-                try:
-                    infos.append(bytes(chaine_bytes).decode("utf-8"))
-                except UnicodeDecodeError:
-                    infos.append(u"")
-            informations = {
-        "num_carte" : infos[0],
-        "date_debut" : infos[2].replace(".","/"),
-        "date_fin" : infos[3].replace(".","/"),
-        "lieu_delivrance" : infos[4],
-        "num_nat" : infos[5],
-        "nom" : infos[6],
-        "prenoms" : infos[7],
-        "suffixe" : infos[8],
-        "nationalite" : infos[9],
-        "lieu_naissance" : infos[10],
-        "date_naissance" : infos[11].split()[0] + "/" + MAP_MOIS[infos[11].split()[1]] + "/" + infos[11].split()[2],
-        "sexe" : infos[12],
-        }
-    
-        # select file : adresse
-        cmd = [0x00, 0xA4, 0x08, 0x0C, len(ADDRESS)] + ADDRESS
-        data, sw1, sw2 = self._sendADPU(cmd)
-    
-        # read file
-        cmd = [0x00, 0xB0, 0x00, 0x00, 256]
-        data, sw1, sw2 = self._sendADPU(cmd)
-        if "%x"%sw1 == "6c":
-            cmd = [0x00, 0xB0, 0x00, 0x00, sw2]
-            data, sw1, sw2 = self._sendADPU(cmd)
-            idx = 0
-            num_info = 0
-            infos = []
-            while num_info <= 2:
-                num_info = data[idx]
-                idx += 1
-                len_info = data[idx]
-                idx += 1
-                chaine_bytes = []
-                for x in range(len_info):
-                    chaine_bytes.append(data[idx])
-                    idx += 1
-                try:
-                    infos.append(bytes(chaine_bytes).decode("utf-8"))
-                except UnicodeDecodeError:
-                    infos.append(u"")
-    
-        informations["adresse"] = infos[0]
-        informations["code_postal"] = infos[1]
-        informations["localite"] = infos[2]
-        
-        if read_photo:
-            # select file : photo
-            cmd = [0x00, 0xA4, 0x08, 0x0C, len(PHOTO)] + PHOTO
-            data, sw1, sw2 = self._sendADPU(cmd)
-    
-            photo_bytes = []
-    
-            offset = 0
-            while "%x"%sw1 == "90":
-                cmd = [0x00, 0xB0, offset, 0x00, 256]
-                data, sw1, sw2 = self._sendADPU(cmd)
-                photo_bytes += data
-                offset += 1
-            if "%x"%sw1 == "6c":
-                offset -= 1
-                cmd = [0x00, 0xB0, offset, 0x00, sw2]
-                data, sw1, sw2 = self._sendADPU(cmd)
-                photo_bytes += data
-                
-            photo = bytearray(photo_bytes)
-            informations["photo"] = photo
-        
-        for (attribute, value) in informations.items():
-            setattr(self, attribute, value)
-
-        return informations
-
+def read_infos(self, read_photo=False):
 
     def _sendADPU(self, apdu):
         response, sw1, sw2 = self._cnx.transmit(apdu)
         return response, sw1, sw2
+
+    self._cnx = self.createConnection()
+    self._cnx.connect()
+    # select file : informations
+    # TODO : manage return codes
+    cmd = [0x00, 0xA4, 0x08, 0x0C, len(ID)] + ID        
+    data, sw1, sw2 = _sendADPU(self, cmd)
+
+    # read file
+    cmd = [0x00, 0xB0, 0x00, 0x00, 256]
+    data, sw1, sw2 = _sendADPU(self, cmd)
+    if "%x"%sw1 == "6c":
+        cmd = [0x00, 0xB0, 0x00, 0x00, sw2]
+        data, sw1, sw2 = _sendADPU(self, cmd)
+        idx = 0
+        num_info = 0
+        infos = []
+        while num_info <= 12:
+            num_info = data[idx]
+            idx += 1
+            len_info = data[idx]
+            idx += 1
+            chaine_bytes = []
+            for x in range(len_info):
+                chaine_bytes.append(data[idx])
+                idx += 1
+            try:
+                infos.append(bytes(chaine_bytes).decode("utf-8"))
+            except UnicodeDecodeError:
+                infos.append(u"")
+        informations = {
+    "num_carte" : infos[0],
+    "date_debut" : infos[2].replace(".","/"),
+    "date_fin" : infos[3].replace(".","/"),
+    "lieu_delivrance" : infos[4],
+    "num_nat" : infos[5],
+    "nom" : infos[6],
+    "prenoms" : infos[7],
+    "suffixe" : infos[8],
+    "nationalite" : infos[9],
+    "lieu_naissance" : infos[10],
+    "date_naissance" : infos[11].split()[0] + "/" + MAP_MOIS[infos[11].split()[1]] + "/" + infos[11].split()[2],
+    "sexe" : infos[12],
+    }
+
+    # select file : adresse
+    cmd = [0x00, 0xA4, 0x08, 0x0C, len(ADDRESS)] + ADDRESS
+    data, sw1, sw2 = _sendADPU(self, cmd)
+
+    # read file
+    cmd = [0x00, 0xB0, 0x00, 0x00, 256]
+    data, sw1, sw2 = _sendADPU(self, cmd)
+    if "%x"%sw1 == "6c":
+        cmd = [0x00, 0xB0, 0x00, 0x00, sw2]
+        data, sw1, sw2 = _sendADPU(self, cmd)
+        idx = 0
+        num_info = 0
+        infos = []
+        while num_info <= 2:
+            num_info = data[idx]
+            idx += 1
+            len_info = data[idx]
+            idx += 1
+            chaine_bytes = []
+            for x in range(len_info):
+                chaine_bytes.append(data[idx])
+                idx += 1
+            try:
+                infos.append(bytes(chaine_bytes).decode("utf-8"))
+            except UnicodeDecodeError:
+                infos.append(u"")
+
+    informations["adresse"] = infos[0]
+    informations["code_postal"] = infos[1]
+    informations["localite"] = infos[2]
+    
+    if read_photo:
+        # select file : photo
+        cmd = [0x00, 0xA4, 0x08, 0x0C, len(PHOTO)] + PHOTO
+        data, sw1, sw2 = _sendADPU(self, cmd)
+
+        photo_bytes = []
+
+        offset = 0
+        while "%x"%sw1 == "90":
+            cmd = [0x00, 0xB0, offset, 0x00, 256]
+            data, sw1, sw2 = _sendADPU(self, cmd)
+            photo_bytes += data
+            offset += 1
+        if "%x"%sw1 == "6c":
+            offset -= 1
+            cmd = [0x00, 0xB0, offset, 0x00, sw2]
+            data, sw1, sw2 = _sendADPU(self, cmd)
+            photo_bytes += data
+            
+        photo = bytearray(photo_bytes)
+        informations["photo"] = photo
+    
+    for (attribute, value) in informations.items():
+        setattr(self, attribute, value)
+
+    return informations
 
 class BeidReader(CardObserver):
 
@@ -155,8 +152,10 @@ class BeidReader(CardObserver):
         (added_cards, removed_cards) = actions
         for card in added_cards:
             if card.reader == self._readername:
-                self.card = BeidCard(self._reader, card)
-                self.on_inserted(card)
+                # attach read_infos method to smartcard.Card.Card instance
+                card.read_infos = MethodType(read_infos, card)
+                self.card = card
+                self.on_inserted(self.card)
         for card in removed_cards:
             if card.reader == self._readername:
                 self.card = None
